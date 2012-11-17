00a494e013affd9e322b6b7178eea655197130b1
Adam Wood <adam.michael.wood@gmail.com>
2012-11-17 10:53:00 -0600

    404 first pass
    404 page using edfw template, Prayer For Lost Pages added to sidebar,
    main content of 404 empty

--- 404.php
+++ 404.php
@@ -8,41 +8,36 @@
  */
 
 get_header(); ?>
-
-	<div id="primary">
-		<div id="content" role="main">
-
-			<article id="post-0" class="post error404 not-found">
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
+<div class="container"> <!-- container for main content and sidebar -->
+	<div class="row"> <!-- row for main content and sidebar -->
+		<div class="span8"> <!-- main content column -->
+			<div class="index-masthead">
+				<header class="page-header">
+					<h2>Can't find page (Error 404)</h2>
 				</header>
-
-				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'twentyeleven' ); ?></p>
-
-					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
-
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'twentyeleven' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
+			</div>
+			<div class="post-list">
+			</div> <!-- post-list -->
+		</div> <!-- /main content column -->
+		<div class="span4"> <!-- sidebar column -->
+			<div class="well"> <!-- sidebar well -->
+				<aside class="widget widget-text">
+					<h3 class="widget-title">Prayer for Lost Pages</h3>
+					<div class="textwidget">
+						<p>O God, who knows and sees all things, grant your faithful servants the wisdom to find those pages that have gone missing.</p>
+						<p>Guide all those who visit our website to the links and pages they seek, and give to all of us the patience needed to endure through the trials brought upon us by the gifts of technology and communication.</p>
+						<p>Help us to be ever grateful for your many blessings in this age, and keep our hearts focused on the blessings that await us in the age to come, when all the lost will be found in your merciful love.</p>
+						<p><b>Amen.</b></p>
 					</div>
+				</aside>
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			</div> <!-- /sidebar well -->
+		</div> <!-- / sidebar column -->
+	</div>	 <!-- /row for main content and sidebar -->
+</div> <!-- /container for main content and sidebar --> 
 
-					<?php
-					/* translators: %1$s: smilie */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'twentyeleven' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', array('count' => 0 , 'dropdown' => 1 ), array( 'after_title' => '</h2>'.$archive_content ) );
-					?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 -->
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
+<!-- <?php // get_sidebar(); ?> -->
+<?php include 'news-and-connections.php'; ?>
 <?php get_footer(); ?>
