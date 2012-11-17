8015f44b13ed0f4a5625445a64a570111311cd5c
Adam Wood <adam.michael.wood@gmail.com>
2012-11-17 14:22:42 -0600

    contact page

---
+++ edfw-content-contact-list.php
@@ -0,0 +1,63 @@
+<h3 class="widget-title">The Episcopal Diocese<br/> of Fort Worth</h3>
+<p>
+4301 Meadowbrook Dr.<br/>
+Fort Worth, TX 76103<br/>
+</p>
+<p>
+Phone: 817-534-1900<br/>
+Fax: 817-534-1904<br/>
+contact@edfw.org
+</p>
+<p>
+Office hours: 10:00 am - 2:00 pm<br/> 
+and other hours by appointment<br/>
+</p>
+
+<dl>
+<dt>Provisional Bishop</dt>
+<dd>The Rt. Rev. Rayford B. High</dd>
+<dd>rayford.high@edfw.org</dd>
+
+<dt>Chancellor</dt>
+<dd>Kathleen Wells </dd>
+<dd>chancellor@edfw.org</dd>
+
+<dt>Transition Ministry Officer</dt>
+<dd>The Rev. Courtland Moore </dd>
+<dd>transition.ministry@edfw.org</dd>
+
+<dt>Ministry Developer | Administrative Officer</dt>
+<dd>Demi Prentiss</dd>
+<dd>demi.prentiss@edfw.org</dd>
+
+<dt>President of the Standing Committee </dt>
+<dd>Marti Fagley </dd>
+<dd>standing.committee.president@edfw.org</dd>
+
+<dt>Operations Manager </dt>
+<dd>Christopher Thomas </dd>
+<dd>operations.manager@edfw.org</dd>
+
+<dt>Bookkeeper</dt> 
+<dd>Cindy Haddock </dd>
+<dd>bookkeeper@edfw.org</dd>
+
+<dt>Registrar</dt> 
+<dd>Cathey Kelly </dd>
+<dd>registrar@edfw.org</dd>
+
+<dt>InReach Coordinator</dt>
+<dd>The Rev. Amy Haynie </dd>
+<dd>inreach@edfw.org</dd>
+
+<dt>Communication Director</dt>
+<dd>Katie Sherrod </dd>
+<dd>katie.sherrod@edfw.org</dd>
+
+<dt>Director of Online Development</dt>
+<dd>Adam Wood</dd>
+<dd>adam.wood@edfw.org </dd>
+
+<dt>Email all other inquiries to</dt> 
+<dd>contact@edfw.org</dd>
+</dl>
---
+++ page-contact.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+<div class="container"> <!-- container for main content and sidebar -->
+	<div class="row"> <!-- row for main content and sidebar -->
+		<div class="span8"> <!-- main content column -->
+			<div class="single-post">
+			<?php if ( have_posts() ) : ?>
+
+								<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+						<div class="page-header">
+							<h1><?php the_title(); ?></h1>
+						</div>
+						<div>
+							<?php include 'post-image-gallery.php'; ?>
+							<div class="row"> <!-- post content below images -->
+								
+								<div class="span8"> <!-- post content -->
+									<?php the_content(); ?>
+								</div> <!-- / post content -->
+							</div> <!-- / post content below images -->
+						</div>
+				<?php endwhile; ?>
+				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+			<?php endif; ?>
+			</div> <!-- post-list -->
+		</div> <!-- /main content column -->
+		<div class="span4"> <!-- sidebar column -->
+			<div class="well"> <!-- sidebar well -->
+				<?php include 'edfw-content-contact-list.php'; ?>
+			</div> <!-- /sidebar well -->
+		</div> <!-- / sidebar column -->
+	</div>	 <!-- /row for main content and sidebar -->
+</div> <!-- /container for main content and sidebar --> 
+
+
+
+<!-- <?php // get_sidebar(); ?> -->
+<?php include 'news-and-connections.php'; ?>
+<?php get_footer(); ?>
