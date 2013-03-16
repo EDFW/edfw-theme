6822ae5b8e56919c7429e99e6f7b08b0ae355428
Adam Wood <adam.michael.wood@gmail.com>
2013-03-16 12:02:05 -0500

    group map display done for now
    info window shows name, address, and link to Google Map for directions

--- edfw-group-map-display.php
+++ edfw-group-map-display.php
@@ -1,8 +1,18 @@
 <?php
 $lat = get_post_meta($post->ID, 'pazzey_lat',true);
 $lng =  get_post_meta($post->ID, 'pazzey_lng',true);
+$streetaddress = get_post_meta($post->ID, 'pazzey_address',true);
+$city = get_post_meta($post->ID, 'pazzey_city',true);
+$state = get_post_meta($post->ID, 'pazzey_state',true);
+$zip = get_post_meta($post->ID, 'pazzey_postal',true);
 if ( ( $lat != '' ) && ( $lng != '' ) ) {
 ?>
+<script type="text/map-content" id="info-window-content">
+	<h5><strong><?php the_title(); ?></strong></h5>
+	<?php echo $streetaddress;?></br>
+	<?php echo $city. ", " .$state. " " . $zip; ?><br/>
+	<a href="https://maps.google.com/maps?z=15&daddr=<?php echo $lat. ','. $lng;?>" target="_new">Get Directions</a>
+</script>
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCgM6ng7sN8kVj71Av_44UQHT_3Ny2WKP8&sensor=false"></script>
 <script type="text/javascript">
 	function initialize() {
@@ -21,7 +31,7 @@ if ( ( $lat != '' ) && ( $lng != '' ) ) {
 		});
 		
 		var infowindow = new google.maps.InfoWindow({
-			content: "<?php the_title(); ?>"
+			content: $("#info-window-content").html()
 		});
 		
 		infowindow.open(map,marker);
